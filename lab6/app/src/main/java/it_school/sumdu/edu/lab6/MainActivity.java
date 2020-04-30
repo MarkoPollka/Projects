34e320ad92933192f6714158be8aee88423e41b9
Vlad Savchenko <polkamaraka@gmail.com>
2020-04-30 13:21:22 +0300

    Initial commit

---
+++ lab6/app/src/main/java/it_school/sumdu/edu/lab6/MainActivity.java
@@ -0,0 +1,21 @@
+package it_school.sumdu.edu.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements
+        ListFragment.OnFragmentInteractionListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onFragmentInteraction(String link) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        if (fragment != null && fragment.isInLayout()) {
+            fragment.setText(link);
+        }
+    }
+}
